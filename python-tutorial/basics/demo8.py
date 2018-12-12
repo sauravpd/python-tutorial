84291bf9f4b5dd6dbe94cc7e4a1200811e6225aa
saurav <sauravpd58@gmail.com>
2018-12-13 02:28:26 +0530

    added demo8.py

---
+++ python-tutorial/basics/demo8.py
@@ -0,0 +1,29 @@
+
+# single line comment
+
+'''
+This is multi
+line
+comment
+'''
+
+a=10
+b=40
+# Addition
+print(a+b)
+
+#Substraction
+print(a-b)
+
+#Multiplication
+print(a*b)
+
+#Division
+print(a/b)
+
+print(a)
+print(b)
+
+#power
+print(2**2)
+
