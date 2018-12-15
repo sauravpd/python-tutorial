cba5a5df1f07c649059ecc29164f7955ddec6f51
saurav <sauravpd58@gmail.com>
2018-12-16 00:31:05 +0530

    added comment

--- python-tutorial/fileHandling/demo1.py
+++ python-tutorial/fileHandling/demo1.py
@@ -2,8 +2,11 @@
 from io import open
 
 text='File Handling example'
+
+#open the file for writing
 file=open('writeFileExample.txt','w')
 
+#write content to the file
 file.write(text)
 
 #close file
