052d6b5a0a5af39ff5a97b9c498d2909d9b605e0
saurav <sauravpd58@gmail.com>
2018-12-19 09:32:01 +0530

    added classes/demo1.py #comment p

---
+++ python-tutorial/classes/demo1.py
@@ -0,0 +1,16 @@
+
+class Calculator:
+    def addition(a,b):
+        add=a+b
+        print(add)
+    def substraction(a,b):
+        sub=a-b
+        print(sub)
+    def multiplication(a, b):
+        mult = a* b
+        print(mult)
+    def division(a, b):
+        div = a/ b
+        print(div)
+
+Calculator.addition(10,10)
