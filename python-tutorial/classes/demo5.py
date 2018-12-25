f755eebbd614ad88b74826ff253bdb1fa33406f7
saurav <sauravpd58@gmail.com>
2018-12-25 15:12:09 +0530

    commit changes in demo5.py

--- python-tutorial/classes/demo5.py
+++ python-tutorial/classes/demo5.py
@@ -1,7 +1,9 @@
 # import two class from statistics example
 from  statistics import mean,variance
 
+#sample data
 data=[2,3,3,4,5,4,3,5,6,7,7,8]
 
+# calling mean and variance function
 print(mean(data))
 print(variance(data))
