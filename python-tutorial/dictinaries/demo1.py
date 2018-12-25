b456cc6357f3d60d7159a6ae6ff2e1b995d4f5a5
saurav <sauravpd58@gmail.com>
2018-12-26 01:44:46 +0530

    added dictionaries/demo1.py

---
+++ python-tutorial/dictinaries/demo1.py
@@ -0,0 +1,13 @@
+
+name={'Steve':25,'Mark':30,'Saurav':22,'Amy':24}
+
+print(name)
+print(name['Steve'])
+
+name['Mike']=25
+print(name)
+name['Mike']=26
+print(name)
+
+del name['Mike']
+print(name)
