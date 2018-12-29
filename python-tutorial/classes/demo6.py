7a394a234e242fe7006385737a74a276e3a1b9de
saurav <sauravpd58@gmail.com>
2018-12-29 21:23:44 +0530

    added comment in demo6.py

--- python-tutorial/classes/demo6.py
+++ python-tutorial/classes/demo6.py
@@ -1,7 +1,9 @@
 # import two class from statistics using alias
 from  statistics import mean as m,variance as v
 
+# sample data
 data=[2,3,3,4,5,4,3,5,6,7,7,8]
 
+# calling function using alias
 print(m(data))
 print(v(data))
