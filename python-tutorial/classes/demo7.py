48c4814378b185bcefd5cbe3dcddccfcd6795126
saurav <sauravpd58@gmail.com>
2018-12-29 21:25:26 +0530

    added comment in demo7.py

--- python-tutorial/classes/demo7.py
+++ python-tutorial/classes/demo7.py
@@ -1,7 +1,9 @@
 # import all classes from statistics
 from  statistics import *
 
+# sample data
 data=[2,3,3,4,5,4,3,5,6,7,7,8]
 
+# calling mean and variance functions
 print(mean(data))
 print(variance(data))
