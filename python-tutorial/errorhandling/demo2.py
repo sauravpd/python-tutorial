9255b5393cf11cd52d2897eacd74eaf153278389
saurav <sauravpd58@gmail.com>
2018-12-30 01:33:05 +0530

    added errorhandling/demo2.py

---
+++ python-tutorial/errorhandling/demo2.py
@@ -0,0 +1,16 @@
+try:
+    print('try block')
+    print('''
+    this is
+    multi
+    line
+    print
+    example
+    +++++++++++++++++++++++++
+    | Multi line  print     |
+    |                       |
+    +++++++++++++++++++++++++
+    ''')
+
+except Exception as e:
+    print(e)
