b2f2ab9e57e05a3d11bbb8ed3f7a1c8f1051d5fd
saurav <sauravpd58@gmail.com>
2019-01-04 23:05:08 +0530

    added functions/demo2.py

---
+++ python-tutorial/functions/demo2.py
@@ -0,0 +1,12 @@
+# function example
+
+# function definition
+def add(num1,num2):
+    print(num1)
+    print(num2)
+    result=num1+num2
+    print("Addition is ",result)
+
+# calling add function
+add(20,40)# Addition is 60
+add(10) # TypeError: add() missing 1 required positional argument: 'num2'
