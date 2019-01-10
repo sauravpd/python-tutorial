52ebfdb83f27ee7667691329f27779fd2d1735e5
saurav <sauravpd58@gmail.com>
2019-01-10 22:10:34 +0530

    added demo7.py

---
+++ python-tutorial/functions/demo7.py
@@ -0,0 +1,18 @@
+num1=5
+num2=-5
+
+print(abs(num1))
+
+print(help(print)) # Help on built-in function print in module builtins:
+
+
+numList=[20,40,10,5,70,80,60]
+
+print(min(numList))
+
+print(max(numList))
+
+a=5.75
+print(round(a))
+
+
