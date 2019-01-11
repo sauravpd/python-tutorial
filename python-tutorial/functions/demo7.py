ffbe2a55077a124d46e81d1202e06f792e607042
saurav <sauravpd58@gmail.com>
2019-01-12 01:05:10 +0530

    added comment in demo7.py

--- python-tutorial/functions/demo7.py
+++ python-tutorial/functions/demo7.py
@@ -3,15 +3,18 @@
 num1=5
 num2=-5
 
+# print abs
 print(abs(num1))
 
 print(help(print)) # Help on built-in function print in module builtins:
 
-
+# number list
 numList=[20,40,10,5,70,80,60]
 
+#print min from numlist
 print(min(numList))
 
+#print max from numList
 print(max(numList))
 
 a=5.75
