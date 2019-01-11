36634c1dd95403a72e2ebcc0eaf81584dd3004ec
saurav <sauravpd58@gmail.com>
2019-01-12 01:06:51 +0530

    added comment

--- python-tutorial/functions/demo8.py
+++ python-tutorial/functions/demo8.py
@@ -1,9 +1,14 @@
 # function  example
+#import math
 import math
 
 a=5.22
+# calling floor function
 print(math.floor(a))
+# calling ceil function
 print(math.ceil(a))
+
+# calling factorial function
 print(math.factorial(math.floor(a)))
 
 
