35a0db81b5b7704621e6f22dd3952f4eb3c134db
saurav <sauravpd58@gmail.com>
2019-01-20 12:48:17 +0530

    added modules/demo5.py

--- python-tutorial/modules/demo5.py
+++ python-tutorial/modules/demo5.py
@@ -1,7 +1,11 @@
+# os and time module example
 import os
 import time
 
+# print os name
 print(os.name)
+# print working directory
 print(os.getcwd())
 
+# make directory name as testDir
 os.mkdir('testDir')
