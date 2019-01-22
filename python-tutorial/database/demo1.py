cd2db87b9abd12b983682348c24e010d4b2e145c
saurav <sauravpd58@gmail.com>
2019-01-22 22:39:57 +0530

    added database/demo1.py

---
+++ python-tutorial/database/demo1.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+connection=sqlite3.connect('testDatabase.db')
+c=connection.cursor()
+
+
+def create_table():
+    c.execute("CREATE TABLE IF NOT EXISTS student(rollno INTEGER,name TEXT)")
+
+def insert_records():
+    c.execute("INSERT INTO student VALUES(1112,'Steve')")
+    connection.commit()
+    c.close()
+    connection.close()
+
+create_table()
+insert_records()
+
