25383c3fe53906c23c504c5602d93a0f9d55800f
saurav <sauravpd58@gmail.com>
2019-01-30 01:59:45 +0530

    commit changes in basics/demo2.py

--- python-tutorial/basics/demo2.py
+++ python-tutorial/basics/demo2.py
@@ -4,10 +4,10 @@ print(type(num))
 
 #Arithmatic Operators
 
-print(3+2)
-print(3-2)
-print(3*2)
-print(3/2)
+print(3+2) # 5
+print(3-2) # 1
+print(3*2) # 6
+print(3/2) 
 print(3//2)
 print(3 ** 2)
 print(3%2)
