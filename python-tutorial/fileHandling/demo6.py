ec8609a87f292f07599e7e777abc0dc436407d6a
saurav <sauravpd58@gmail.com>
2019-02-02 01:18:31 +0530

    commit changes in demo6.py

--- python-tutorial/fileHandling/demo6.py
+++ python-tutorial/fileHandling/demo6.py
@@ -1,9 +1,13 @@
+# file handling example
 import csv
 
+# open data.csv file
 with open('data.csv') as csvfile:
     names=[]
+	# read data from csv
     readcsv=csv.reader(csvfile,delimiter=',')
     for row in  readcsv:
         name=row[0]
         names.append(name)
+# print the list of names
 print(names)
