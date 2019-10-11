1fde1521101a0afc6ba6795df6f292aaf65f2802
saurav <sauravpd58@gmail.com>
2019-10-11 23:48:35 +0530

    added types.py

---
+++ python-tutorial/basics/types.py
@@ -0,0 +1,13 @@
+x = '48'
+y = int(x)
+
+print(f'x is {type(x)}')
+print(f'x is {x}')
+print(f'y is {type(y)}')
+print(f'y is {y}')
+
+# output
+#x is <class 'str'>
+#x is 48
+#y is <class 'int'>
+#y is 48
